906dadbce144b143da2ebbb05d2753f8b2160cfc
GregoryAtom22 <62352393+GregoryAtom22@users.noreply.github.com>
2022-04-12 23:51:36 +0600

    Commit test

---
+++ My project/Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    [SerializeField] private Transform levelPart_1;
+    private void Awake()
+    {
+        SpawnLevelPart(new Vector3(1, 2));
+        SpawnLevelPart(new Vector3(1, 2) + new Vector3(3, 1));
+        SpawnLevelPart(new Vector3(3, 2) + new Vector3(3+2, 1));
+    }
+    private void SpawnLevelPart(Vector3 spawnPosition)
+    {
+        Instantiate(levelPart_1, spawnPosition, Quaternion.identity);
+    }
+}
